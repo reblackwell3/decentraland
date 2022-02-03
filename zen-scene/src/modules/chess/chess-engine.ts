be8980c8f7b31791d7161e9f5d4cf687df9d54a6
Robert Blackwell <reblackwell3@gmail.com>
2022-02-03 16:39:50 -0500

    first crack at minimal chess engine

--- zen-scene/src/modules/chess/chess-engine.ts
+++ zen-scene/src/modules/chess/chess-engine.ts
@@ -1,5 +1,22 @@
 import {GameEngine} from "../game-int";
+import {Chess} from "chess.js";
 
 export class ChessEngine extends GameEngine {
+
+    private chess = new Chess();
+
+    isValid(move: String): boolean {
+        return this.chess.moves().includes(move)
+    }
+
+    //engine will return null if move fails
+    move(move: String): boolean {
+        return this.chess.move(move) != null
+    }
+
+    //this method will be for debugging purposes
+    ascii(): String {
+        return this.chess.ascii()
+    }
 
 }
